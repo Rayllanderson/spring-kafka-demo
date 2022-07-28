03ad83d04982a86fe0d1a47c0356e83ac6b14019
Rayllanderson <rayllanderson@gmail.com>
2022-07-28 20:23:25 -0300

    criando interface kafkaconsumer

--- consumer/src/main/java/com/rayllanderson/kafka/consumer/product/ProductConsumer.java
+++ consumer/src/main/java/com/rayllanderson/kafka/consumer/product/ProductConsumer.java
@@ -1,29 +1,28 @@
 package com.rayllanderson.kafka.consumer.product;
 
+import com.rayllanderson.kafka.consumer.kafka.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
-import static org.springframework.kafka.support.KafkaHeaders.CORRELATION_ID;
-
 @Service
-public class KafkaListenerService {
+public class ProductConsumer implements KafkaConsumer<String, Product> {
 
-    private final Logger log = LoggerFactory.getLogger(KafkaListenerService.class);
+    private final Logger log = LoggerFactory.getLogger(ProductConsumer.class);
     private final ProductRepository repository;
 
-    public KafkaListenerService(ProductRepository repository) {
+    public ProductConsumer(ProductRepository repository) {
         this.repository = repository;
     }
 
+    @Override
     @KafkaListener(topics = "${spring.kafka.product.topic}", groupId = "${spring.kafka.consumer.group-id}")
-    public void listen(@Payload ConsumerRecord<String, Product> product, @Header(CORRELATION_ID) String correlationId, Acknowledgment ack) {
-        log.info("Mensagem recebida no tópico, {}, correlation-id={}", product, correlationId);
+    public void listen(@Payload ConsumerRecord<String, Product> product, Acknowledgment ack) {
+        log.info("Mensagem recebida no tópico, {}", product);
 
         repository.save(product.value());
         log.info("Produto {} salvo com sucesso", product.value().getId());
