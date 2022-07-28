bcdc9c9de668ad6ce25c40b104e5e0b7538d097f
Rayllanderson <rayllanderson@gmail.com>
2022-07-28 20:12:24 -0300

    ajustando consumer

--- consumer/src/main/java/com/rayllanderson/kafka/consumer/config/KafkaConfiguration.java
+++ consumer/src/main/java/com/rayllanderson/kafka/consumer/config/KafkaConfiguration.java
@@ -1,48 +1,40 @@
 package com.rayllanderson.kafka.consumer.config;
 
 import com.rayllanderson.kafka.consumer.product.Product;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import java.util.HashMap;
-import java.util.Map;
+import static org.springframework.kafka.listener.ContainerProperties.AckMode.MANUAL_IMMEDIATE;
 
 @Configuration
 public class KafkaConfiguration {
-    private final KafkaProperties kafkaProperties;
-
-    public KafkaConfiguration(KafkaProperties kafkaProperties) {
-        this.kafkaProperties = kafkaProperties;
-    }
-
-    public Map<String, Object> consumerConfigurations() {
-        Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, kafkaProperties.getConsumer().getKeyDeserializer());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, kafkaProperties.getConsumer().getValueDeserializer());
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getConsumer().getGroupId());
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getConsumer().getAutoOffsetReset());
-        return properties;
-    }
+    private static final Logger log = LoggerFactory.getLogger(KafkaConfiguration.class);
 
     @Bean
-    public ConsumerFactory<String, Product> productConsumerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, Product> kafkaListenerContainerFactory(ConsumerFactory<String, Product> productConsumerFactory) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, Product>();
+        factory.setConsumerFactory(createConsumerFactoryBasedOn(productConsumerFactory));
+        factory.getContainerProperties().setAckMode(MANUAL_IMMEDIATE);
+        factory.setErrorHandler(errorHandler());
+        factory.setConcurrency(3);
+        return factory;
+    }
+
+    private DefaultKafkaConsumerFactory<String, Product> createConsumerFactoryBasedOn(ConsumerFactory<String, Product> productConsumerFactory) {
         StringDeserializer stringDeserializer = new StringDeserializer();
         var jsonDeserializer = new JsonDeserializer<>(Product.class, false);
-        return new DefaultKafkaConsumerFactory<>(consumerConfigurations(), stringDeserializer, jsonDeserializer);
+        return new DefaultKafkaConsumerFactory<>(productConsumerFactory.getConfigurationProperties(), stringDeserializer, jsonDeserializer);
     }
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Product> kafkaListenerContainerFactory() {
-        var factory = new ConcurrentKafkaListenerContainerFactory<String, Product>();
-        factory.setConsumerFactory(productConsumerFactory());
-        return factory;
+    private ErrorHandler errorHandler() {
+        return (exception, consumerRecord) -> log.error("erro ao consumir mensagem, {}-{}", exception.getMessage(), consumerRecord, exception);
     }
 }
